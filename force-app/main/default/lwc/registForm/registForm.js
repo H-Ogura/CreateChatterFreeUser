7771ebdb4455175c3df0a6eae06d8fbd462818e8
小倉 裕之 <ogurah@fsi.co.jp>
2019-09-12 18:57:14 +0900

    パスワードのリセット機能を追加

--- force-app/main/default/lwc/registForm/registForm.js
+++ force-app/main/default/lwc/registForm/registForm.js
@@ -1,25 +1,29 @@
 import { LightningElement, track } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 
 export default class RegistForm extends LightningElement {
     @track userId;
     @track username;
     @track ailas;
+    @track canreset = false;
 
     handleSuccess(event) {
-       this.userId = event.detail.id;
-       this.dispatchEvent(
-            new ShowToastEvent({
-                title: 'Success',
-                message: event.detail.apiName + ' created.',
-                variant: 'success',
-            }),
-        );
+        this.userId = event.detail.id;
+        this.canreset = true;
     }
 
     handleEmailChange(event){
         const username = event.detail.value.split('@');
         this.ailas = username[0];
         this.username = username[0] + '@sf.fsi.co.jp';
+    }
+
+    navigateToWebPage() {
+        this[NavigationMixin.Navigate]({
+            "type": "standard__webPage",
+            "attributes": {
+                "url": "https://login.salesforce.com/secur/forgotpassword.jsp?locale=jp&un=" + this.username
+            }
+        });
     }
 }
