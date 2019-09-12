ace1f0d1b1d5fafa1ad64ccd6bd832424ac4e890
小倉 裕之 <ogurah@fsi.co.jp>
2019-09-12 13:58:50 +0900

    一応完成

--- force-app/main/default/aura/auraregistApp/auraregistApp.app
+++ force-app/main/default/aura/auraregistApp/auraregistApp.app
@@ -1,3 +1,3 @@
-<aura:application extends="ltng:outApp">
+<aura:application access="GLOBAL" extends="ltng:outApp" implements="ltng:allowGuestAccess">
     <aura:dependency resource="c:auraregistForm"></aura:dependency>
 </aura:application>	
--- force-app/main/default/lwc/registForm/registForm.js
+++ force-app/main/default/lwc/registForm/registForm.js
@@ -1,8 +1,22 @@
 import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class RegistForm extends LightningElement {
-    @track accountId;
+    @track userId;
+    @track username;
     handleSuccess(event) {
-       this.accountId = event.detail.id;
+       this.userId = event.detail.id;
+       this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: event.detail.apiName + ' created.',
+                variant: 'success',
+            }),
+        );
+    }
+
+    handleEmailChange(event){
+        const username = event.detail.value.split('@');
+        this.username = username[0] + '@sf.fsi.co.jp';
     }
 }
