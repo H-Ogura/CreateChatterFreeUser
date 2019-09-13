1f63926e01881f5a8ed0d9e5ed03c1800954a7f0
小倉 裕之 <ogurah@fsi.co.jp>
2019-09-13 13:07:08 +0900

    プロファイルに自動でIdをセットしたくて格闘したが断念。Apexは残しておく。

--- force-app/main/default/lwc/registForm/registForm.js
+++ force-app/main/default/lwc/registForm/registForm.js
@@ -1,11 +1,18 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
+import chatterProfileId from '@salesforce/apex/registFormController.chatterProfileId';
 
 export default class RegistForm extends LightningElement {
     @track userId;
     @track username;
     @track ailas;
     @track canreset = false;
+    @track chatterid;
+
+    @wire(chatterProfileId)
+    getchatterId(data){
+        this.chatterid = data;
+    } 
 
     handleSuccess(event) {
         this.userId = event.detail.id;
